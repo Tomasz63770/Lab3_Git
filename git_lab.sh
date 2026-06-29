4abf05dc477acca735170cb1d8eefdb35128c21b
Tomasz63770 <tomasz63770@gmail.com>
2026-06-29 21:55:59 +0200

    Update git_lab.sh
    Expanded the git_lab.sh script with various Git commands for different levels of usage, including branching, merging, rebasing, and remote repository operations.

--- git_lab.sh
+++ git_lab.sh
@@ -1,9 +1,69 @@
 #!/bin/bash
 
-# =========================
-# LOCAL - INTRODUCTION
-# =========================
-git init
-git status
-git add .
-git commit -m "commit"
+git commit
+git commit
+
+# Pozioom 2
+git branch bugFix
+git checkout bugFix
+
+# Poziom 3
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+# Poziom 4: Git Rebase
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+
+# CZĘŚĆ 2: Poruszanie się po repozytorium
+
+# Poziom 1: Odczepianie HEAD (Detach HEAD)
+git checkout C2
+
+# Poziom 2: Względne referencje (Relative Refs ^)
+git checkout bugFix^
+
+# Poziom 3: Względne referencje 2 (Relative Refs ~)
+git branch -f main C6
+git checkout HEAD~1
+git branch -f bugFix C0
+
+# Poziom 4: Odwracanie zmian w Gicie (Git Reset / Git Revert)
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+
+# CZĘŚĆ 3: Przenoszenie pracy (Advanced Git)
+
+# Poziom 1: Cherry Pick
+git cherry-pick C3 C4 C7
+
+# Poziom 2: Interaktywny Rebase (Git Interactive Rebase)
+git rebase -i HEAD~4
+# (W tutorialu otworzy się okno interaktywne, gdzie zmieniasz kolejność lub usuwasz commity)
+
+# CZĘŚĆ 4: Zdalne repozytoria (Remote)
+
+# Poziom 1: Git Clone
+git clone
+
+# Poziom 2: Git Fetch
+git fetch
+
+# Poziom 3: Git Pull
+git fetch
+git merge o/main
+
+# Poziom 4: Git Push
+git commit
+git commit
+git push
